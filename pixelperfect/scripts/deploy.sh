6b03e6c6e3d621cfa92e8333fe4306712693b1f1
Nathan Broadbent <git@ndbroadbent.com>
2018-12-30 20:46:43 +0700

    Added a deploy script for pixelperfect

---
+++ pixelperfect/scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+if ! [ -d deploy ]; then
+  echo "deploy directory does not exist."
+  set -x
+  git clone git@gitlab.com:form_api/pixel_perfect.git deploy
+  cd deploy
+  git checkout build
+  set +x
+  echo "Cloned the repo. Please try again."
+  exit 1
+fi
+
+set -x
+
+craco build
+rm -rf deploy/static/ deploy/*
+cp -R build/* deploy/
+cd deploy
+git add -A
+git commit -m "Update"
+git push
